b6862af6fddc25b9ac2fa5a751897ff8989bba0f
sean00880 <theelitereapersanctum@gmail.com>
2022-12-27 23:01:55 -0500

    ui update3

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Index() {
                             <div className="content_holder2">
                             <div className="contact_left">
 								<div className="container3">
-								<Marquee className='marquee-list-container' gradientColor={0}> <ul className="marquee-list"><li>Commercial Cleanout</li><li>Property Cleanout</li><li>Storage Cleanout</li><li>Construction Debris Removal</li><li>Appliance Removal</li><li>Shed Removal</li><li>Deck Removal</li><li>Junk Removal</li></ul></Marquee>
+								<Marquee className='marquee-list-container' gradientColor={0} speed={20}> <ul className="marquee-list" aria-hidden="true"><li>Commercial Cleanout</li><li>Property Cleanout</li><li>Storage Cleanout</li><li>Construction Debris Removal</li><li>Appliance Removal</li><li>Shed Removal</li><li>Deck Removal</li><li>Junk Removal</li></ul></Marquee>
                                 </div>
 									<h3>Receive Your FREE Instant Quote!</h3>
 									<form className="contact_form" action="/" method="post" autoComplete="off" data-email="frenifyteam@gmail.com">
